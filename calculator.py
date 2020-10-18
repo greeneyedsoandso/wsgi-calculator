10fc4c6ebe3b60b847863b794cd4d0d8e8246287
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-10-18 10:50:02 -0700

    Added boilerplate from books exercise

--- calculator.py
+++ calculator.py
@@ -25,20 +25,6 @@ Consider the following URL/Response body pairs as tests:
   http://localhost:8080/divide/22/11   => 2
   http://localhost:8080/               => <html>Here's how to use this page...</html>
 ```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
 """
 
 
@@ -52,6 +38,7 @@ def add(*args):
     return sum
 
 # TODO: Add functions for handling more arithmetic operations.
+
 
 def resolve_path(path):
     """
@@ -68,17 +55,31 @@ def resolve_path(path):
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]    #
+    # TODO (bonus): Add error handling for a user attempting to divide by zero.
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
